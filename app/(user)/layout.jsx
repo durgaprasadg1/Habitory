927bc6eaf47c93ce34f383e57358e55aef0d7662
Durgaprasad <durgaprasadgirepunje@gmail.com>
2026-02-20 09:05:26 +0530

    login only toast added

--- app/(user)/layout.jsx
+++ app/(user)/layout.jsx
@@ -3,7 +3,6 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 export default async function UserLayout({ children }) {
   const { userId } = await auth();
-
   if (!userId) {
     redirect("/sign-in");
   }
--- app/components/User/LoginToast.jsx
+++ app/components/User/LoginToast.jsx
@@ -3,17 +3,18 @@
 import { useEffect, useRef } from "react";
 import { useUser } from "@clerk/nextjs";
 import { toast } from "sonner";
-
+import { usePathname } from "next/navigation";
 export default function LoginToast() {
   const { user, isLoaded } = useUser();
+  const path = usePathname();
   const hasShownToast = useRef(false);
 
   useEffect(() => {
     if (isLoaded && user && !hasShownToast.current) {
       hasShownToast.current = true;
-      toast.success(
+      {path === "/dashboard" && toast.success(
         `Welcome , ${user.firstName || user.username || "User"}! 🎉`,
-      );
+      );}
     }
   }, [isLoaded, user]);
 
